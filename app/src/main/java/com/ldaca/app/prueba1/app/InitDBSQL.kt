4b224909ed5fd2ed042672880445cf3583c399b0
Ldaca <Ldaca77@outlook.com>
2020-10-09 10:54:52 -0500

    Cambios: * Stetho

---
+++ app/src/main/java/com/ldaca/app/prueba1/app/InitDBSQL.kt
@@ -0,0 +1,30 @@
+package com.ldaca.app.prueba1.app
+
+import android.app.Application
+import com.facebook.stetho.Stetho
+import com.uphyca.stetho_realm.RealmInspectorModulesProvider
+import io.realm.Realm
+import io.realm.RealmConfiguration
+
+class InitDBSQL : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        initRealm()
+        val realm = Realm.getDefaultInstance()
+        val realmInspector = RealmInspectorModulesProvider.builder(this)
+            .withDeleteIfMigrationNeeded(true)
+            .build()
+        Stetho.initialize(
+            Stetho.newInitializerBuilder(this)
+            .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
+            .enableWebKitInspector(realmInspector)
+            .build()
+        )
+    }
+
+    private fun initRealm() {
+        Realm.init(this)
+        val config = RealmConfiguration.Builder().deleteRealmIfMigrationNeeded().build()
+        Realm.setDefaultConfiguration(config)
+    }
+}
